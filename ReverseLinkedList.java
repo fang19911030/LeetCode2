1222c23eea87f9030fe65d8b20cf6d085e459936
fang19911030 <fang291831388@gmail.com>
2017-07-25 14:05:58 -0400

    Reverse Linked List

---
+++ ReverseLinkedList.java
@@ -0,0 +1,18 @@
+public class ReverseLinkedList {
+    /**
+     * @param head: The head of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
