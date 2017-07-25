e0caa3ec7df9a44fc52ce959aca823ba9b8ce25d
fang19911030 <fang291831388@gmail.com>
2017-07-25 10:39:02 -0400

    LeetCode 19 LintCode 174

---
+++ RemoveNthNodeFromEndOFList.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    ListNode removeNthFromEnd(ListNode head, int n) {
+        // write your code here
+        
+        ListNode h1 = reverse(head);
+        ListNode h2 = remove(h1,n);
+        return reverse(h2);
+    }
+    
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode cur = head;
+        while( cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+    
+    private ListNode remove(ListNode head, int n){
+        ListNode help = new ListNode(0);
+        help.next = head;
+        ListNode cur = help;
+        for(int i=1;i<n;i++){
+            cur = cur.next;
+        }
+        
+        cur.next = cur.next.next;
+        return help.next;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length  = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
