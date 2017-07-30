334ea59b1f001ce1377374531861bd27f22deb32
fang19911030 <fang291831388@gmail.com>
2017-07-30 19:05:01 -0400

    Update

--- SortList.java
+++ SortList.java
@@ -13,7 +13,6 @@ public class Solution {
     /**
      * @param head: The head of linked list.
      * @return: You should return the head of the sorted linked list,
-                    using constant space complexity.
      */
     public ListNode sortList(ListNode head) {  
         // write your code here
