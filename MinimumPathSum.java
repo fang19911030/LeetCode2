b284f2544667565e7d39e736894fefd6ed7a8000
fang19911030 <fang291831388@gmail.com>
2017-08-19 13:45:19 -0400

    Minimum Path Sum

---
+++ MinimumPathSum.java
@@ -0,0 +1,32 @@
+public class Solution {
+    /*
+     * @param grid: a list of lists of integers
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    public int minPathSum(int[][] grid) {
+        // write your code here
+        if(grid == null || grid.length==0){
+            return 0;
+        }
+        int r = grid.length;
+        int c = grid[0].length;
+        
+        int[][] dp = new int[r][c];
+        dp[0][0] = grid[0][0];
+        for(int i=1;i<r;i++){
+            dp[i][0] = grid[i][0]+dp[i-1][0];
+        }
+        
+        for(int j=1;j<c;j++){
+            dp[0][j] = grid[0][j]+dp[0][j-1];
+        }
+        
+        for(int i=1;i<r;i++){
+            for(int j=1;j<c;j++){
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+        
+        return dp[r-1][c-1];
+    }
+};
