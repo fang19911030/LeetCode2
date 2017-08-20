cc3e08a97faa0a9245480805a5626f62f411ccce
fang19911030 <fang291831388@gmail.com>
2017-08-20 15:03:25 -0400

    backpack

---
+++ Backpack.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     */
+    public int backPack(int m, int[] A) {
+        // write your code here
+        int[][] dp = new int[A.length+1][m+1];
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                if(j>=A[i-1]){
+                    dp[i][j] =Math.max(dp[i-1][j-A[i-1]]+A[i-1], dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+
+        }
+        
+        return dp[A.length][m];
+    }
+}
