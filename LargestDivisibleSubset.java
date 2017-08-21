540a6ac9507e7f9fc0a575100655853f6f746091
fang19911030 <fang291831388@gmail.com>
2017-08-21 16:44:24 -0400

    Old version is not correct

--- LargestDivisibleSubset.java
+++ LargestDivisibleSubset.java
@@ -1,26 +1,34 @@
-public class Solution {
-    /**
-     * @param nums a set of distinct positive integers
-     * @return the largest subset 
-     */
+import java.util.*;
+public class LargestDivisbleSubset {
     public List<Integer> largestDivisibleSubset(int[] nums) {
         // Write your code here
-        if(nums == null ){
-            return null;
+        if(nums == null || nums.length == 0){
+            return new ArrayList<Integer>();
+        }
+        if(nums.length == 1){
+            List<Integer> res = new ArrayList<>();
+            res.add(nums[0]);
+            return res;
         }
         List<Integer>[] res = (List<Integer>[]) new List[nums.length];
-        
+
         Arrays.sort(nums);
-        
+
         res[0] = new ArrayList<>();
         res[0].add(nums[0]);
-        
+
         for(int i=1;i<nums.length;i++){
             for(int j=i-1;j>=0;j--){
                 if(nums[i] % nums[j] == 0){
-                    res[i] = new ArrayList<>(res[j]);
-                    res[i].add(nums[i]);
-                    break;
+                    List<Integer> cur = new ArrayList<>(res[j]);
+                    cur.add(nums[i]);
+                    if(res[i] == null){
+                        res[i] = cur;
+                    }else{
+                        if(cur.size()>res[i].size()){
+                            res[i] = cur;
+                        }
+                    }
                 }
             }
             if(res[i] == null){
@@ -40,4 +48,3 @@ public class Solution {
         }
         return r;
     }
-}
