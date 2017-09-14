598eb29507cb24f0ab9d7103f3647776f082b41b
fang19911030 <fang291831388@gmail.com>
2017-09-14 15:24:46 -0400

    LFUCachse (important)

---
+++ LFUCache.java
@@ -0,0 +1,111 @@
+class LFUCache {
+    private Node head;
+    private int cap = 0;
+    private HashMap<Integer, Integer> valueHash = null;
+    private HashMap<Integer, Node> nodeHash = null;
+    
+    public LFUCache(int capacity) {
+        this.cap = capacity;
+        valueHash = new HashMap<Integer, Integer>();
+        nodeHash = new HashMap<Integer, Node>();
+    }
+    
+    public int get(int key) {
+        if(valueHash.containsKey(key)){
+            increaseCount(key);
+            return valueHash.get(key);
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value){
+        if( cap == 0) return;
+        if(valueHash.containsKey(key)){
+            valueHash.put(key,value);
+        } else {
+            if(valueHash.size() < cap){
+                valueHash.put(key, value);
+            }else{
+                removeOld();
+                valueHash.put(key, value);
+                
+            }
+            addToHead(key);
+        }
+        increaseCount(key);
+    }
+    
+    private void addToHead(int key){
+        if(head == null){
+            head = new Node(0);
+            head.keys.add(key);
+        }else if(head.count > 0){
+            Node node = new Node(0);
+            node.keys.add(key);
+            node.next = head;
+            head.pre = head;
+            head = node;
+        }else{
+            head.keys.add(key);
+        }
+        nodeHash.put(key, head);
+    }
+    
+    private void increaseCount(int key){
+        Node node = nodeHash.get(key);
+        node.keys.remove(key);
+        if(node.next == null){
+            node.next = new Node(node.count+1);
+            node.next.pre = node;
+            node.next.keys.add(key);
+        }else if(node.next.count == node.count+1){
+            node.next.keys.add(key);
+        }else{
+            Node tmp = new Node(node.count+1);
+            tmp.keys.add(key);
+            tmp.pre = node;
+            tmp.next = node.next;
+            node.next.pre = tmp;
+            node.next = tmp;
+        }
+        nodeHash.put(key,node.next);
+        if(node.keys.size() == 0) remove(node);
+    }
+    
+    private void removeOld(){
+        if(head == null) return;
+        int old = 0;
+        for(int n:head.keys){
+            old = n;
+            break;
+        }
+        head.keys.remove(old);
+        if(head.keys.size() == 0) remove(head);
+        nodeHash.remove(old);
+        valueHash.remove(old);
+    }
+    
+    private void remove(Node node){
+        if(node.pre == null){
+            head = node.next;
+        }else {
+            node.pre.next = node.next;
+        }
+        if(node.next != null){
+            node.next.pre = node.pre;
+        }
+    }
+    
+    class Node{
+        public int count = 0;
+        public LinkedHashSet<Integer> keys = null;
+        public Node pre = null;
+        public Node next = null;
+        public Node(int count){
+            this.count = count;
+            keys = new LinkedHashSet<Integer>();
+            pre = next = null;
+        }
+    }
+
+}
