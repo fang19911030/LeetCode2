60d7bd2555a4a9d97a28e876c67f786f1ba3bd12
fang19911030 <fang291831388@gmail.com>
2018-01-10 10:24:17 -0500

    662

---
+++ 662MaximumWidthOfBinaryTree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 0, 1, new ArrayList<Integer>(), new ArrayList<>());
+    }
+    
+    private int dfs(TreeNode node, int level, int order, List<Integer> start, List<Integer> end){
+        if(root == null) return 0;
+        if(start.size() == level){
+            start.add(order);
+            end.add(order);
+        }else{
+            end.set(level, order);
+        }
+        int cur = end.get(level)- start.get(level) + 1;
+        int left = dfs(root.left, level+1, order*2, start, end);
+        int right = dfs(root.right, level+1, 2*order+1, start, end);
+        return Math.max(cur, Math.max(left, right));
+    }
+}
