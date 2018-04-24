28b3baf212cb3630601d93a278e5397f6b889646
Pengcheng Fang <pxf109@case.edu>
2018-04-24 11:20:45 -0400

    Need to analysis

--- PatchingArray.java
+++ PatchingArray.java
@@ -1,52 +1,32 @@
 class Solution {
     public int minPatches(int[] nums, int n) {
-        if ( n == 0){
-            return 0;
-        }
-        int res = 0;
-        List<Integer> num = new ArrayList<>();
-        if(nums == null || nums.length == 0){
-            num.add(1);
-            res++;
-        }else{
-            for (Integer t:nums){
-                num.add(t);
-            }
-        }
+        int miss = 1, added = 0, i = 0;
         
-        Set<Integer> sums = new HashSet<>();
-
-        while(sums.size()<n){
-            for(int i=1; i<= num.size();i++){
-                for(int j=0; j<=num.size() -i;j++){
-                    getSum(num,i,sums,j,0);
-                }
+        while(miss <=n ){
+            if(i < nums.length && nums[i] <= miss){
+                miss += nums[i++];
+            }else{
+                miss += miss;
+                added++;
             }
-            System.out.println(sums);
-            for(int i = 1;i<=n;i++){
-                if(!sums.contains(i)){
-                    num.add(i);
-                    res++;
-                    break;
-                }
-            }
-            System.out.println(num);
         }
-        return res;
+        return added;
     }
-    
-    private void getSum(List<Integer> num, int k, Set<Integer> set, int idx, int sum){
-        if(k==0){
-            set.add(sum);
-            return;
+}
+
+
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        int miss = 1, added = 0, i = 0;
+        
+        while(miss <=n ){
+            if(i < nums.length && nums[i] <= miss){
+                miss += nums[i++];
+            }else{
+                miss += miss;
+                added++;
+            }
         }
-        if(idx >=num.size()){
-            return;
-        }
-        sum += num.get(idx);
-        for(int i = idx+1; i <= num.size();i++){
-            getSum(num, k-1,set,i,sum);
-        }
-        sum -=num.get(idx);        
+        return added;
     }
 }
